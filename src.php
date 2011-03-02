36208c506425e573d51e13b62bb140b636978117
Paul M. Jones <pmjones88@gmail.com>
2011-03-01 20:34:51 -0600

    add an all-files src.php file

---
+++ src.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/src/ConfigInterface.php';
+require_once __DIR__ . '/src/Config.php';
+require_once __DIR__ . '/src/Container.php';
+require_once __DIR__ . '/src/Exception.php';
+require_once __DIR__ . '/src/Exception/ServiceInvalid.php';
+require_once __DIR__ . '/src/Exception/ServiceNotFound.php';
+require_once __DIR__ . '/src/ForgeInterface.php';
+require_once __DIR__ . '/src/Forge.php';
+require_once __DIR__ . '/src/Lazy.php';
