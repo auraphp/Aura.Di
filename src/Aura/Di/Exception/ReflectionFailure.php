4af66f3d70cc4d3aa68770d81160a507baa79154
Paul M. Jones <pmjones88@gmail.com>
2013-09-09 17:21:40 -0500

    update docblocks:

--- src/Aura/Di/Exception/ReflectionFailure.php
+++ src/Aura/Di/Exception/ReflectionFailure.php
@@ -1,8 +1,24 @@
 <?php
+/**
+ * 
+ * This file is part of the Aura Project for PHP.
+ * 
+ * @package Aura.Di
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
 namespace Aura\Di\Exception;
 
 use Aura\Di\Exception;
 
+/**
+ * 
+ * There was a failure when reflecting on a class.
+ * 
+ * @package Aura.Di
+ * 
+ */
 class ReflectionFailure extends Exception
 {
 }
