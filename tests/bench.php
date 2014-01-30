fdb871a6682ae9a48237c1bd4f38acecdbe2e117
Paul M. Jones <pmjones88@gmail.com>
2014-01-30 15:06:27 -0600

    add a bench script

---
+++ tests/bench.php
@@ -0,0 +1,27 @@
+<?php
+use Aura\Di\Container;
+use Aura\Di\Factory;
+
+require '../autoload.php';
+
+class Foo {}
+
+class Bar extends Foo {}
+
+class Baz extends Bar {}
+
+class Dib extends Baz {}
+
+class Zim extends Dib {}
+
+class Gir extends Zim {}
+
+$di = new Container(new Factory);
+$k = 10000;
+
+$before = microtime(true);
+for ($i = 0; $i < $k; $i ++) {
+    $gir = $di->newInstance('Gir');
+}
+$after = microtime(true);
+echo $after - $before . PHP_EOL;
