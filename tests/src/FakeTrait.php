ed20a702b5c422d6ed6bd41ebc5714c3504fc3b9
djmattyg007 <git@matthewgamble.net>
2014-04-20 11:59:37 +1000

    Added tests for child trait setters.

--- tests/src/FakeTrait.php
+++ tests/src/FakeTrait.php
@@ -3,13 +3,15 @@ namespace Aura\Di;
 
 trait FakeTrait
 {
+    use FakeChildTrait;
+
     protected $fake;
 
     public function setFake($fake)
     {
         $this->fake = $fake;
     }
-    
+
     public function getFake()
     {
         return $this->fake;
