0a104f3032119b158cd04f45c33ebf90d9ab5d57
Paul M. Jones <pmjones88@gmail.com>
2015-03-26 14:47:31 -0500

    do not auto-run composer, per @harikt

--- phpunit.php
+++ phpunit.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 $autoloader = __DIR__ . '/vendor/autoload.php';
 if (! file_exists($autoloader)) {
-    passthru('composer install');
+    echo "Composer autoloader not found: $autoloader" . PHP_EOL;
+    echo "Please issue 'composer install' and try again." . PHP_EOL;
+    exit(1);
 }
 require $autoloader;
