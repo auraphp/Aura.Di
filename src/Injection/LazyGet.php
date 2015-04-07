3ca3a9a0e2577dad0133360bc2ea39ea3b6ad35f
David Négrier <d.negrier@thecodingmachine.com>
2015-04-07 14:03:00 +0200

    Adding support for container-interop's delegate dependency lookup concept

--- src/Injection/LazyGet.php
+++ src/Injection/LazyGet.php
@@ -9,6 +9,7 @@
 namespace Aura\Di\Injection;
 
 use Aura\Di\Container;
+use Interop\Container\ContainerInterface;
 
 /**
  *
@@ -41,12 +42,12 @@ class LazyGet implements LazyInterface
      *
      * Constructor.
      *
-     * @param Container $container The service container.
+     * @param ContainerInterface $container The service container.
      *
      * @param string $service The service to retrieve.
      *
      */
-    public function __construct(Container $container, $service)
+    public function __construct(ContainerInterface $container, $service)
     {
         $this->container = $container;
         $this->service = $service;
