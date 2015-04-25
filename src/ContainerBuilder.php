c6a0eb6ee6aa02b9d0b0d7be8aefa35e4b4d7f80
takayuki_yamaguchi <takayuki_yamaguchi@dwango.co.jp>
2015-04-25 16:51:14 +0900

    git add `throws_on_auto_resolve_failed` option argument to ContainerBuiler

--- src/ContainerBuilder.php
+++ src/ContainerBuilder.php
@@ -37,6 +37,24 @@ class ContainerBuilder
 
     /**
      *
+     * Enable throws error on auto-resolution failed.
+     *
+     * @const true
+     *
+     */
+    const ENABLE_THROWS_ON_AUTO_RESOLVE_FAILED = true;
+
+    /**
+     *
+     * Enable throws error on auto-resolution failed.
+     *
+     * @const false
+     *
+     */
+    const DISABLE_THROWS_ON_AUTO_RESOLVE_FAILED = false;
+
+    /**
+     *
      * Creates a new DI container, adds pre-existing service objects, applies
      * Config classes to define() services, locks the container, and applies
      * the Config instances to modify() services.
@@ -50,16 +68,21 @@ class ContainerBuilder
      * @param bool $auto_resolve Enable or disable auto-resolve after the
      * define() step?
      *
+     * @param bool $throws_on_auto_resolve_failed Enables or disables throws
+     * error on auto-resolution failed.
+     *
      * @return Container
      *
      */
     public function newInstance(
         array $services = array(),
         array $config_classes = array(),
-        $auto_resolve = self::ENABLE_AUTO_RESOLVE
+        $auto_resolve = self::ENABLE_AUTO_RESOLVE,
+        $throws_on_auto_resolve_failed = self::DISABLE_THROWS_ON_AUTO_RESOLVE_FAILED
     ) {
         $di = new Container(new Factory);
         $di->setAutoResolve($auto_resolve);
+        $di->setThrowsOnAutoResolveFailed($throws_on_auto_resolve_failed);
 
         foreach ($services as $key => $val) {
             $di->set($key, $val);
