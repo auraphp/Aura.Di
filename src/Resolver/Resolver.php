13defdeb6b9d41cfa3009cfb78318dd644c08de8
Frederik Bosch <f.bosch@genkgo.nl>
2019-01-04 09:16:29 +0100

    fix php 5.5

--- src/Resolver/Resolver.php
+++ src/Resolver/Resolver.php
@@ -431,14 +431,12 @@ class Resolver
     public function getExpandedParams($class, array $params)
     {
         // Variadics are only available in PHP >= 5.6, and not in HHVM
-        if (version_compare(PHP_VERSION, '5.6') === -1 || defined('HHVM_VERSION')) {
-            return $params;
-        }
+        $variadicsEnabled = version_compare(PHP_VERSION, '5.6') === -1 || defined('HHVM_VERSION');
 
         $variadicParams = [];
         foreach ($this->reflector->getParams($class) as $reflectParam) {
             $paramName = $reflectParam->getName();
-            if ($reflectParam->isVariadic() && is_array($params[$paramName])) {
+            if ($variadicsEnabled && $reflectParam->isVariadic() && is_array($params[$paramName])) {
                 $variadicParams = array_merge($variadicParams, $params[$paramName]);
                 unset($params[$paramName]);
                 break; // There can only be one
